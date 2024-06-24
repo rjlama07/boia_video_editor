79dad01b2b20a960c0ad9ebe981f9f954caa92de
Flutter Developer <flutterdev77@gmail.com>
2024-06-24 19:57:31 +0545

    Fix bouncy issue

--- lib/src/widgets/trim/trim_slider.dart
+++ lib/src/widgets/trim/trim_slider.dart
@@ -306,104 +306,112 @@ class _TrimSliderState extends State<TrimSlider>
   //GESTURES//
   //--------//
   void _onHorizontalDragStart(DragStartDetails details) {
-    final pos = details.localPosition;
-    final progressTrim = _getVideoPosition();
+    if (_scrollController.position.outOfRange) {
 
-    // Left, right and video progress indicator touch areas
-    Rect leftTouch = Rect.fromCenter(
-      center: Offset(_rect.left - _edgesTouchMargin / 2, _rect.height / 2),
-      width: _edgesTouchMargin,
-      height: _rect.height,
-    );
-    Rect rightTouch = Rect.fromCenter(
-      center: Offset(_rect.right + _edgesTouchMargin / 2, _rect.height / 2),
-      width: _edgesTouchMargin,
-      height: _rect.height,
-    );
-    final progressTouch = Rect.fromCenter(
-      center: Offset(progressTrim, _rect.height / 2),
-      width: _positionTouchMargin,
-      height: _rect.height,
-    );
+    } else {
+      final pos = details.localPosition;
+      final progressTrim = _getVideoPosition();
 
-    // if the scroll view is touched, it will be by default an inside gesture
-    _boundary = _TrimBoundaries.inside;
+      // Left, right and video progress indicator touch areas
+      Rect leftTouch = Rect.fromCenter(
+        center: Offset(_rect.left - _edgesTouchMargin / 2, _rect.height / 2),
+        width: _edgesTouchMargin,
+        height: _rect.height,
+      );
+      Rect rightTouch = Rect.fromCenter(
+        center: Offset(_rect.right + _edgesTouchMargin / 2, _rect.height / 2),
+        width: _edgesTouchMargin,
+        height: _rect.height,
+      );
+      final progressTouch = Rect.fromCenter(
+        center: Offset(progressTrim, _rect.height / 2),
+        width: _positionTouchMargin,
+        height: _rect.height,
+      );
 
-    /// boundary should not be set to other that inside when scroll controller is moving
-    /// it would lead to weird behavior to change position while scrolling
-    if (isNotScrollBouncingBack &&
-        !_scrollController.position.isScrollingNotifier.value) {
-      if (progressTouch.contains(pos)) {
-        // video indicator should have the higher priority since it does not affect the trim param
-        _boundary = _TrimBoundaries.progress;
-      } else {
-        // if video indicator is not touched, expand [leftTouch] and [rightTouch] on the inside
-        leftTouch = leftTouch.expandToInclude(
-            Rect.fromLTWH(_rect.left, 0, _edgesTouchMargin, 1));
-        rightTouch = rightTouch.expandToInclude(Rect.fromLTWH(
-            _rect.right - _edgesTouchMargin, 0, _edgesTouchMargin, 1));
+      // if the scroll view is touched, it will be by default an inside gesture
+      _boundary = _TrimBoundaries.inside;
+
+      /// boundary should not be set to other that inside when scroll controller is moving
+      /// it would lead to weird behavior to change position while scrolling
+      if (isNotScrollBouncingBack &&
+          !_scrollController.position.isScrollingNotifier.value) {
+        if (progressTouch.contains(pos)) {
+          // video indicator should have the higher priority since it does not affect the trim param
+          _boundary = _TrimBoundaries.progress;
+        } else {
+          // if video indicator is not touched, expand [leftTouch] and [rightTouch] on the inside
+          leftTouch = leftTouch.expandToInclude(
+              Rect.fromLTWH(_rect.left, 0, _edgesTouchMargin, 1));
+          rightTouch = rightTouch.expandToInclude(Rect.fromLTWH(
+              _rect.right - _edgesTouchMargin, 0, _edgesTouchMargin, 1));
+        }
+
+        if (leftTouch.contains(pos)) {
+          _boundary = _TrimBoundaries.left;
+        } else if (rightTouch.contains(pos)) {
+          _boundary = _TrimBoundaries.right;
+        }
       }
 
-      if (leftTouch.contains(pos)) {
-        _boundary = _TrimBoundaries.left;
-      } else if (rightTouch.contains(pos)) {
-        _boundary = _TrimBoundaries.right;
-      }
+      _updateControllerIsTrimming(true);
     }
-
-    _updateControllerIsTrimming(true);
   }
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    final Offset delta = details.delta;
-    final posLeft = _rect.topLeft + delta;
+    if (_scrollController.position.outOfRange) {
 
-    switch (_boundary) {
-      case _TrimBoundaries.left:
-        final clampLeft = posLeft.dx.clamp(_horizontalMargin, _rect.right);
-        // avoid rect to be out of bounds & avoid minTrim to be bigger than maxTrim
-        _changeTrimRect(
-            left: clampLeft,
-            width: _rect.width - (clampLeft - posLeft.dx).abs() - delta.dx);
-        break;
-      case _TrimBoundaries.right:
-      // avoid rect to be out of bounds & maxTrim to be smaller than minTrim
-        _changeTrimRect(
-          width: (_rect.left + _rect.width + delta.dx)
-              .clamp(_rect.left, _trimLayout.width + _horizontalMargin) -
-              _rect.left,
-        );
-        break;
-      case _TrimBoundaries.inside:
-        if (_isExtendTrim) {
-          _scrollController.position.moveTo(
-            _scrollController.offset - delta.dx,
-            clamp: false,
-          );
-        } else {
-          // avoid rect to be out of bounds
+    } else {
+      final Offset delta = details.delta;
+      final posLeft = _rect.topLeft + delta;
+
+      switch (_boundary) {
+        case _TrimBoundaries.left:
+          final clampLeft = posLeft.dx.clamp(_horizontalMargin, _rect.right);
+          // avoid rect to be out of bounds & avoid minTrim to be bigger than maxTrim
           _changeTrimRect(
-            left: posLeft.dx.clamp(
-              _horizontalMargin,
-              _trimLayout.width + _horizontalMargin - _rect.width,
-            ),
+              left: clampLeft,
+              width: _rect.width - (clampLeft - posLeft.dx).abs() - delta.dx);
+          break;
+        case _TrimBoundaries.right:
+        // avoid rect to be out of bounds & maxTrim to be smaller than minTrim
+          _changeTrimRect(
+            width: (_rect.left + _rect.width + delta.dx)
+                .clamp(_rect.left, _trimLayout.width + _horizontalMargin) -
+                _rect.left,
           );
-        }
-        break;
-      case _TrimBoundaries.progress:
-        final pos = details.localPosition.dx;
-        // postion of pos on the layout width between 0 and 1
-        final localRatio = pos / (_trimLayout.width + _horizontalMargin * 2);
-        // because the video progress cursor is on a different layout context (horizontal margin are not applied)
-        // the gesture offset must be adjusted (remove margin when localRatio < 0.5 and add margin when localRatio > 0.5)
-        final localAdjust = (localRatio - 0.5) * (_horizontalMargin * 2);
-        _controllerSeekTo((pos + localAdjust).clamp(
-          _rect.left - _horizontalMargin,
-          _rect.right + _horizontalMargin,
-        ));
-        break;
-      default:
-        break;
+          break;
+        case _TrimBoundaries.inside:
+          if (_isExtendTrim) {
+            _scrollController.position.moveTo(
+              _scrollController.offset - delta.dx,
+              clamp: false,
+            );
+          } else {
+            // avoid rect to be out of bounds
+            _changeTrimRect(
+              left: posLeft.dx.clamp(
+                _horizontalMargin,
+                _trimLayout.width + _horizontalMargin - _rect.width,
+              ),
+            );
+          }
+          break;
+        case _TrimBoundaries.progress:
+          final pos = details.localPosition.dx;
+          // postion of pos on the layout width between 0 and 1
+          final localRatio = pos / (_trimLayout.width + _horizontalMargin * 2);
+          // because the video progress cursor is on a different layout context (horizontal margin are not applied)
+          // the gesture offset must be adjusted (remove margin when localRatio < 0.5 and add margin when localRatio > 0.5)
+          final localAdjust = (localRatio - 0.5) * (_horizontalMargin * 2);
+          _controllerSeekTo((pos + localAdjust).clamp(
+            _rect.left - _horizontalMargin,
+            _rect.right + _horizontalMargin,
+          ));
+          break;
+        default:
+          break;
+      }
     }
   }
 
@@ -656,9 +664,11 @@ class _TrimSliderState extends State<TrimSlider>
                         builder: (_, __) {
                           // Adjust the rect to account for the horizontal padding
                           final paddedRect = Rect.fromLTRB(
-                            _rect.left + widget.framePadding, // Add left padding
+                            _rect.left + widget.framePadding,
+                            // Add left padding
                             _rect.top,
-                            _rect.right - widget.framePadding, // Add right padding
+                            _rect.right - widget.framePadding,
+                            // Add right padding
                             _rect.bottom,
                           );
 
